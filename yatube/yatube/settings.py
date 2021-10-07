74e27161d68f99c79ed746b2b4e2b2a91c3b25db
kotofey97 <olya_sitnikova@mail.ru>
2021-10-07 14:29:20 +0300

    рефакторинг шаблонов

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
 
+from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,17 +20,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'svpb(u%s(05y*uqi6@7qhv$2c4mu&n3vwdg&loc*zf__xn_huo'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '[::1]',
     'testserver',
+    # 'www.hellocowboy.pythonanywhere.com',
+    # 'hellocowboy.pythonanywhere.com',
 ]
 
 CACHES = {
